767a9cf728079d1e34c9a8c39ad3ad8c533b2b58
ארטיום אברמוב <artiom240648@gmail,com>
2024-07-21 21:24:59 +0300

    upload

--- server/Routes/boardRoute.js
+++ server/Routes/boardRoute.js
@@ -2,10 +2,10 @@ const express = require("express");
 const router = express.Router();
 const boardController = require("../Controllers/boardController");
 const upload = require("../middleware/multerMiddleware");
-const authenticateRoute = require("../middleware/authenticateRoute");
+// const authenticateRoute = require("../middleware/authenticateRoute");
 
 router.get("/getBoardItems", boardController.getBoardItems);
-router.use(authenticateRoute);
+// router.use(authenticateRoute);
 router.get("/findBoardItem", boardController.findBoardItem);
 router.post("/createBoard", upload, boardController.createBoard);
 router.put("/updateBoard/:id", upload, boardController.updateBoardItem);
--- server/Routes/programRoute.js
+++ server/Routes/programRoute.js
@@ -1,14 +1,14 @@
 const express = require("express");
 const router = express.Router();
 const programController = require("../Controllers/programController");
-const authenticateRoute = require("../middleware/authenticateRoute");
+// const authenticateRoute = require("../middleware/authenticateRoute");
 
 router.get(
   "/getProgramByType/:programType",
   programController.getProgramByType
 );
 
-router.use(authenticateRoute);
+// router.use(authenticateRoute);
 
 router.post(
   "/createProgram",
